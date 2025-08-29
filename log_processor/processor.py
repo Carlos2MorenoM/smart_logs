480708c582e5d638192e0442826e70c4d369593a
Carlos Moreno M. <c.morenomontesdeoca@gmail.com>
2025-08-29 21:14:40 +0200

    [SLOGS-0] - Add Elasticsearch connection

--- log_processor/processor.py
+++ log_processor/processor.py
@@ -1,27 +1,49 @@
 import pika
 import json
+from elasticsearch import Elasticsearch
 
+# Configure and connect Elasticsearch
+def connect_to_es():
+    try:
+        es = Elasticsearch(
+            ['http://elasticsearch:9200'],
+            verify_certs=False,
+        )
+        return es
+    except Exception as e:
+        print(f"Error connecting to Elasticsearch: {e}")
+        return None
+
+# Define processor function
 def callback(ch, method, properties, body):
-    """
-    Executes when a message is received
-    """
+    try:
+        log_entry = json.loads(body)
+        print(f"✅ New log received and processed: {log_entry}")
 
-    log_entry = json.loads(body)
+        es_client.index(index="logs", document=log_entry)
+        print("📦 Log stored on Elasticsearch")
 
-    print(f" New log received and processed {log_entry}")
+        # Notify RabbitMQ message was processed
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+    except Exception as e:
+        print(f"❌ Error processing log message: {e}")
 
-    # Notify RabbitMQ that the message is processed
-    ch.basic_ack(delivery_tag=method.delivery_tag)
+if __name__ == '__main__':
+    # Connect Elasticsearch
+    es_client = connect_to_es()
+    if es_client is None:
+        exit(1)
 
+    # Connect RabbitMQ
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
     channel = connection.channel()
 
-    # Queue exists
+    # Keeping sure queue exists
     channel.queue_declare(queue='logs', durable=True)
 
-    # Consumer for 'logs' queue
-    print(" [*] Waiting for logs. Exit using CTRL+C")
-    channel.basic_consume(queue='logs', on_message_callback=callback)
+    # Configure the consumer
+    channel.basic_consume(queue='logs', on_message_callback=callback, auto_ack=False)
 
-    # Start messages receiver loop
+    print(" [*] Waiting for logs. Exit using CTRL+C")
+    # Consume messages
     channel.start_consuming()
